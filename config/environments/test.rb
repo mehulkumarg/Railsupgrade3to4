44e235e3bfdc6d97f64db59c97eb9e04f9b7c2e0
Mehul Gurjar <mehulkumarg@cybage.com>
2014-06-25 15:12:54 +0530

    Rails3.1 to Rails3.2

--- config/environments/test.rb
+++ config/environments/test.rb
@@ -39,5 +39,4 @@ RailsUpgrade3to4::Application.configure do
 
   # Allow pass debug_assets=true as a query parameter to load pages with unpackaged assets
   config.assets.allow_debugging = true
-
 end

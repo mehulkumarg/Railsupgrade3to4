ec4e1b4f80f530713ba925fd7814da3521cb1248
Mehul Gurjar <mehulkumarg@cybage.com>
2014-06-25 14:40:34 +0530

    +initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+RailsUpgrade3to4::Application.config.secret_token = '4bbda3e19f65250e29afa6943407f0e797891aaae24a3a0c7591f52a2270395aea4d429009350f2d81a893a577be175d14f5a0457c473926f63c70226c4c86d5'
